c43d0346ac779dd61f6b63ff908b785b714af573
Germán Loera <german.loera@4thsource.com>
2018-07-27 17:18:30 -0500

    QuickSort

--- DataStructures/src/com/ger/datastructures/Main.java
+++ DataStructures/src/com/ger/datastructures/Main.java
@@ -7,10 +7,10 @@ import java.util.Random;
 public class Main {
 
 
-    public static void printArray (String algoritmo, int[] array){
+    public static void printArray(String algoritmo, int[] array) {
 
         System.out.print(algoritmo + ": ");
-        for(int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
@@ -19,13 +19,13 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	// write your code here
-        int[] array = {10,5,89 ,21,-4,-62 ,8,45, 66, -9,12, 22, 58, -1,-12};
+        // write your code here
+        int[] array = {10, 5, 89, 21, -4, -62, 8, 45, 66, -9, 12, 22, 58, -1, -12};
 
         printArray("Original", array);
 
-      //  ShellSort.Sort(array, array.length/2);
-        MergeSort.Sort(array);
-
+        //  ShellSort.Sort(array, array.length/2);
+        //  MergeSort.Sort(array);
+        // QuickSort.Sort(array);
     }
 }
