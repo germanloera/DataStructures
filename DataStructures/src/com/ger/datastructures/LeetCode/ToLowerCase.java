a678267bef03c34a37aae832a160c552bafd4e3d
Germán Loera <german.loera@4thsource.com>
2018-08-15 18:01:48 -0500

    2252

---
+++ DataStructures/src/com/ger/datastructures/LeetCode/ToLowerCase.java
@@ -0,0 +1,33 @@
+package com.ger.datastructures.LeetCode;
+
+import com.ger.datastructures.Main;
+
+public class ToLowerCase {
+    public static final int a = (int) 'a';
+    public static final int A = (int) 'A';
+    public static final int space = a - A;
+
+    public static String toLowerCase(String str) {
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            int c = (int) str.charAt(i);
+            if(c >= 'A' && c <= 'Z'){
+                char lowerCase = (char) (c + space);
+                result.append(lowerCase);
+            }else{
+                result.append((char) c);
+            }
+
+
+
+        }
+        return result.toString();
+    }
+    public static void main(String[] args){
+
+        Main.p(toLowerCase("al&phaBET"));//"Germán Raúl Loera Velasco"));
+
+    }
+
+}
